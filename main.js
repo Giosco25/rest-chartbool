daece42dea7461534b2f47a005d11a6a9be77bfd
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-06-10 17:35:05 +0200

    fine grafico 1

--- main.js
+++ main.js
@@ -4,57 +4,110 @@ $(document).ready(function(){
      'url': 'http://157.230.17.132:4028/sales',
      'method': 'GET',
      'success': function(data){
-            console.log(data);
-        var clienti = {};
-        console.log(clienti);
+         // creo un array con tutti i mesi
+        var vendite_mesi ={
+            'gennaio': 0,
+            'febbraio':0,
+            'marzo':0,
+            'aprile':0,
+            'maggio':0,
+            'giugno':0,
+            'luglio':0,
+            'agosto':0,
+            'settembre':0,
+            'ottobre':0,
+            'novembre':0,
+            'dicembre':0
+        };
+
 
         for (var i = 0; i < data.length; i++) {
             // mi salvo le informazioni dei clienti
             var informazioni_clienti = data[i];
+            console.log(informazioni_clienti);
             // console.log(informazioni_clienti);
-
+            // salvo nomi dei clienti per la parte 2 della milestone 1
             var nomi_clienti = informazioni_clienti.salesman;
-            console.log(nomi_clienti);
             // salvo in una variabile il capitale dei clienti
             var capitale_clienti = informazioni_clienti.amount;
-            console.log(capitale_clienti);
+            // salvo la data di ogni cliente
             var  date = informazioni_clienti.date;
-            console.log(date);
+            moment.locale('it');
+           var mesi_clienti = moment(date, "DD/MM/YYYY").format("MMMM");
+           // console.log(data_clienti);
+           vendite_mesi[mesi_clienti] += capitale_clienti;
 
-        // moment(date, "DD-MM-YYYY");
-        // console.log(moment);
-        if (!clienti.hasOwnProperty(capitale_clienti)) {
-            clienti[capitale_clienti] = date;
-
-        }else {
-            clienti[capitale_clienti] += nomi_clienti;
-        }
-        console.log("sono il cliente:" + clienti);
-          // }
-        // if(!clienti.hasOwnProperty(informazioni_clienti)) {
-        //
-        //     clienti[informazioni_clienti] = capitale_clienti;
-        // } else {
-        //
-        //     clienti[informazioni_clienti] += capitale_clienti;
-        // }
-        // console.log('Capitale :'+ capitale_clienti);
-        // console.log('informazioni :' + date);
         }; // fine for
+        console.log(vendite_mesi);
+        // estraggo le chiavi dell'oggetto continenti
+        // queste corrispondono ai nomi dei continenti
+        var mesi = Object.keys(vendite_mesi);
+        console.log(mesi);
+
+        // estraggo i valori dell'oggetto continenti
+        // questi corrispondono al totale dei dischi venduti per continente
+        var capitale = Object.values(vendite_mesi);
+        // console.log(capitale);
+        var ctx = $('#chart-uno')[0].getContext('2d');
+
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: mesi,
+                datasets: [{
+                    label: 'Period 1',
+                    data: capitale,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 35, 12, 1)',
+                        'rgba(54, 200, 27, 1)',
+                        'rgba(255, 206, 324, 1)',
+                        'rgba(52, 1, 192, 1)',
+                        'rgba(153, 41, 255, 1)',
+                        'rgba(2, 159, 64, 1)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 35, 12, 1)',
+                        'rgba(54, 200, 27, 1)',
+                        'rgba(255, 206, 324, 1)',
+                        'rgba(52, 1, 192, 1)',
+                        'rgba(153, 41, 255, 1)',
+                        'rgba(2, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                }]
+            },
+            options: {
+                legend:{
+                    position: 'right',
+                    fontColor: '#333'
+                },
+                title: {
+                    display: true,
+                    text: 'Fatturato Mensile',
+                    fontSize: 50
+                }
+            }
+        });
 
      } // fine success
 
 
     });// fine ajax
 
+
+
 });// fine document ready
-//
-// $.ajax({
-//    'url':'https://flynn.boolean.careers/exercises/api/array/music',
-//    'method': 'GET',
-//    'success': function(data) {
-//        // console.log(data);
-//        // Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
-//        var musica = data.response;
-//        // console.log(musica)
+
 //        var array_generi =[];
