76cc00ae0988a5306104cfd3263a1268cf05bb73
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-06-11 16:58:18 +0200

    Sistemazione 2 grafico

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 $(document).ready(function(){
     // chiamata API per recuperare i dati
     $.ajax({
-     'url': 'http://157.230.17.132:4028/sales',
-     'method': 'GET',
-     'success': function(vendite_mensili){
-         preparazione_dati_vendite(vendite_mensili)
-
-     } // fine success
+        'url': 'http://157.230.17.132:4028/sales',
+        'method': 'GET',
+        'success': function(vendite_mensili){
+            preparazione_dati_vendite(vendite_mensili)
+            vendite_venditori(vendite_mensili);
+        } // fine success
     });// fine ajax
-   function preparazione_dati_vendite(dati){
-       // creo un array con tutti i mesi
-      var vendite_mesi = {
-          'gennaio': 0,
-          'febbraio':0,
-          'marzo':0,
-          'aprile':0,
-          'maggio':0,
-          'giugno':0,
-          'luglio':0,
-          'agosto':0,
-          'settembre':0,
-          'ottobre':0,
-          'novembre':0,
-          'dicembre':0
-      };
 
-      for (var i = 0; i < dati.length; i++) {
-          // mi salvo le informazioni dei clienti
+    function preparazione_dati_vendite(dati){
+        // creo un array con tutti i mesi
+        var vendite_mesi = {
+            'gennaio': 0,
+            'febbraio':0,
+            'marzo':0,
+            'aprile':0,
+            'maggio':0,
+            'giugno':0,
+            'luglio':0,
+            'agosto':0,
+            'settembre':0,
+            'ottobre':0,
+            'novembre':0,
+            'dicembre':0
+        };
+
+        for (var i = 0; i < dati.length; i++) {
+            // mi salvo le informazioni dei clienti
             var informazioni_clienti = dati[i];
             // salvo in una variabile il capitale dei clienti
             var capitale_clienti = informazioni_clienti.amount;
@@ -36,16 +37,16 @@ $(document).ready(function(){
             var mesi_clienti = moment(date, "DD/MM/YYYY").format("MMMM");
             // console.log(data_clienti);
             vendite_mesi[mesi_clienti] += capitale_clienti;
-            disegna_grafico_vendite(vendite_mesi);
         } // fine for
+        disegna_grafico_vendite(vendite_mesi);
+    } //** function preparazione dati vendite **//
 
-   } //** function preparazione dati vendite **//
-
-   function vendite_venditori(dati){
+    function vendite_venditori(dati){
         var vendite_venditori = {};
 
+        var vendite_totali = 0;
         for (var i = 0; i < dati.length; i++) {
-           // mi salvo le informazioni dei clienti
+            // mi salvo le informazioni dei clienti
             var informazioni_clienti = dati[i];
             // salvo in una variabile il capitale dei clienti
             var capitale_clienti = informazioni_clienti.amount;
@@ -54,27 +55,40 @@ $(document).ready(function(){
             if (vendite_venditori.hasOwnProperty(nomi_clienti)) {
                 // la chiave per questo venditore è già definita
                // incremento il suo totale con l'importo della vendita corrente
-                vendite_venidtori[nome_clienti] += capitale_clienti
-            }else {
+                vendite_venditori[nomi_clienti] += capitale_clienti
+            } else {
                 // la chiave con il nome di questo venditore non esiste
                 // non ho ancora incontrato questo vendtore in nessuna iterazione precedente
-                vendite_venidtori[nome_clienti] = capitale_clienti
+                vendite_venditori[nomi_clienti] = capitale_clienti
             }
-            disegna_vendite_venditori(vendite_venditori);
-   }
-   function disegna_grafico_vendite(dati_vendite_mensili){
-       // console.log(vendite_mesi);
-       // estraggo i mesi dell'oggetto vendite mesi
-       // queste corrispondono ai nomi dei mesi
-       var mesi = Object.keys(dati_vendite_mensili);
-       console.log(mesi);
-       // estraggo il capitale dell'oggetto vendite_mesi
-       // questi corrispondono al totale del capitale di ogni mese
-       var capitale = Object.values(dati_vendite_mensili);
-       // console.log(capitale);
-       var ctx = $('#grafico_vendite')[0].getContext('2d');
+            vendite_totali += capitale_clienti
+        } // fine ciclo for
+        for (var nome_venditore in vendite_venditori) {
+            // salvo in una variabile l'importo di ogni singolo venditore
+            var importo_singolo_venditore = vendite_venditori[nome_venditore];
+            // calcolo la percentuale di ogni singolo venditore
+            var percentuale_singolo_venditore = (importo_singolo_venditore * 100/ vendite_totali).toFixed(1);
 
-       var myChart = new Chart(ctx, {
+            vendite_venditori[nome_venditore] = percentuale_singolo_venditore;
+
+        }
+        console.log(vendite_venditori);
+        disegna_grafico_vendite_venditori(vendite_venditori);
+    } //** fine funzione vendite venditori
+
+    function disegna_grafico_vendite(dati_vendite_mensili){
+        // console.log(vendite_mesi);
+        // estraggo i mesi dell'oggetto vendite mesi
+        // queste corrispondono ai nomi dei mesi
+        var mesi = Object.keys(dati_vendite_mensili);
+        // console.log(mesi);
+        // estraggo il capitale dell'oggetto vendite_mesi
+        // questi corrispondono al totale del capitale di ogni mese
+        var capitale = Object.values(dati_vendite_mensili);
+        // console.log(capitale);
+        var ctx = $('#grafico_vendite')[0].getContext('2d');
+
+        var myChart = new Chart(ctx, {
            type: 'line',
            data: {
                labels: mesi,
@@ -97,51 +111,43 @@ $(document).ready(function(){
                    display: true,
                    text: 'Fatturato Mensile',
                    fontSize: 50
-                }
+               } // fine title
             } // fine options
        });// fine my chart
    } //** function disegna grafico vendite **//
 
-function disegna_vendite_venditori(dati_vendite_venditori){
-    var nomi_venditori = Object.keys(dati_vendite_venditori);
-    console.log(mesi);
-    // estraggo il capitale dell'oggetto vendite_mesi
-    // questi corrispondono al totale del capitale di ogni mese
-    var dati_venditori = Object.values(dati_vendite_venditori);
-    // console.log(capitale);
-    var ctx = $('#chart-due')[0].getContext('2d');
+    function disegna_grafico_vendite_venditori(dati_vendite_venditori){
+       var nomi_venditori = Object.keys(dati_vendite_venditori);
+        // estraggo il capitale dell'oggetto vendite_mesi
+        // questi corrispondono al totale del capitale di ogni mese
+        var dati_venditori = Object.values(dati_vendite_venditori);
 
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-   data: {
-       labels: nomi_venditori,
-       datasets: [{
-           label: '# of Votes',
-           data: dati_venditori,
-           backgroundColor: [
-               'rgba(255, 99, 132, 0.2)',
-               'rgba(54, 162, 235, 0.2)',
-               'rgba(255, 206, 86, 0.2)',
-               'rgba(75, 192, 192, 0.2)',
-           ],
-           borderColor: [
-               'rgba(255, 99, 132, 1)',
-               'rgba(54, 162, 235, 1)',
-               'rgba(255, 206, 86, 1)',
-               'rgba(75, 192, 192, 1)',
-           ],
-           borderWidth: 1
-       }]
-   },
-   options: {
-       scales: {
-           yAxes: [{
-               ticks: {
-                   beginAtZero: true
-               }
-           }]
-       }
-   }
-   }); // fine chart
- };//** funzione disegna vendite venditori **//
+        var ctx = $('#grafico_venditori')[0].getContext('2d');
+
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: nomi_venditori,
+                datasets: [{
+                    label: '# of Votes',
+                    data: dati_venditori,
+                    backgroundColor: [
+                       'rgba(255, 99, 132, 0.2)',
+                       'rgba(54, 162, 235, 0.2)',
+                       'rgba(255, 206, 86, 0.2)',
+                       'rgba(75, 192, 192, 0.2)',
+                    ],
+                    borderColor: [
+                       'rgba(255, 99, 132, 1)',
+                       'rgba(54, 162, 235, 1)',
+                       'rgba(255, 206, 86, 1)',
+                       'rgba(75, 192, 192, 1)',
+                    ],
+                    borderWidth: 1
+                }] // fine datasets
+            }, // fine data
+        }); // fine chart
+    };//** funzione disegna vendite venditori **//
+
+    ///****** MILESTONE 2 ******///
 });// fine document ready
